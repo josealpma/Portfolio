1d0a0238271e1292dcfc3453a14bfd27f5a8a090
Jose Ponce <joseal.pma@outlook.com>
2022-07-06 09:10:46 -0500

    Complementadas secciones resume / Education / Portfolio / contact

--- src/app/Views/portfolio/portfolio.component.ts
+++ src/app/Views/portfolio/portfolio.component.ts
@@ -9,6 +9,16 @@ export class PortfolioComponent implements OnInit {
 
   constructor() { }
 
+  Items: any = [
+    {
+      Title: 'Dashboard',
+      Description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit omnis quae corporis veritatis unde quam optio sunt enim. Fugit voluptatibus sit, quis assumenda odit aperiam. Quos quisquam magni officiis!',
+      Languages: '',
+      Url: '',
+      Img: '',
+    }
+  ];
+
   ngOnInit(): void {
   }
 
